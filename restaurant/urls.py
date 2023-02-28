64014ccb230b6ac4f854b114a0bf17e363cbae26
Gabriele Ardizzone <gabri.ardi@gmail.com>
2023-02-28 18:29:03 +0100

    add MenuItem and SingleMenuItem views

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from . import serializers
+from .models import Menu, Booking
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 # Create your views here.
+
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
