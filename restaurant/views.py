11aef13bc3c7f98ae80d0c0fb4ae6d3d498aa4cd
Gabriele Ardizzone <gabri.ardi@gmail.com>
2023-02-28 23:35:01 +0100

    add authentication to views

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,19 +3,23 @@ from .serializers import MenuSerializer, BookingSerializer
 from .models import Menu, Booking
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class MenuItemView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
