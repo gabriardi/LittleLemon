105f17bedb6e29664ddea6bf270b0f70f6e6bb79
Gabriele Ardizzone <gabri.ardi@gmail.com>
2023-02-28 18:39:45 +0100

    add BookingViewSet

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from . import serializers
+from .serializers import MenuSerializer, BookingSerializer
 from .models import Menu, Booking
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
 
 class MenuItemView(ListCreateAPIView):
     queryset = Menu.objects.all()
-    serializer_class = serializers.MenuSerializer
+    serializer_class = MenuSerializer
 
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
-    serializer_class = serializers.MenuSerializer
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
